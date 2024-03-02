a48c1830ffd10d275c5b014d31a426688f7f8ba5
XxShuoxX <Shuo.Hu-1@student.uts.edu.au>
2024-03-02 23:41:57 +1100

    Interactable Blocks
    Interactable Blocks (just the block)

--- lab2/Assets/Script/PlayerMovement.cs
+++ lab2/Assets/Script/PlayerMovement.cs
@@ -3,55 +3,143 @@
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
-    private new Rigidbody2D rigidbody2D;
-    private new Collider2D collider2D;
-
-    public float moveSpeed = 8f;
-    public float jumpForce = 15f;
+    private new Camera camera;
+    private new Rigidbody2D rigidbody;
+    private new Collider2D collider;
 
     private Vector2 velocity;
-    private bool isJumping;
+    private float inputAxis;
+
+    public float moveSpeed = 8f;
+    public float maxJumpHeight = 5f;
+    public float maxJumpTime = 1f;
+    public float jumpForce => (2f * maxJumpHeight) / (maxJumpTime / 2f);
+    public float gravity => (-2f * maxJumpHeight) / Mathf.Pow(maxJumpTime / 2f, 2f);
+
+    public bool grounded { get; private set; }
+    public bool jumping { get; private set; }
+    public bool running => Mathf.Abs(velocity.x) > 0.25f || Mathf.Abs(inputAxis) > 0.25f;
+    public bool sliding => (inputAxis > 0f && velocity.x < 0f) || (inputAxis < 0f && velocity.x > 0f);
+    public bool falling => velocity.y < 0f && !grounded;
 
     private void Awake()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();
-        collider2D = GetComponent<Collider2D>();
+        camera = Camera.main;
+        rigidbody = GetComponent<Rigidbody2D>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void OnEnable()
+    {
+        rigidbody.isKinematic = false;
+        collider.enabled = true;
+        velocity = Vector2.zero;
+        jumping = false;
+    }
+
+    private void OnDisable()
+    {
+        rigidbody.isKinematic = true;
+        collider.enabled = false;
+        velocity = Vector2.zero;
+        jumping = false;
     }
 
     private void Update()
     {
-        MovePlayer();
-        Jump();
-    }
+        HorizontalMovement();
 
-    private void MovePlayer()
-    {
-        float moveInput = Input.GetAxis("Horizontal");
-        velocity = new Vector2(moveInput * moveSpeed, rigidbody2D.velocity.y);
-        rigidbody2D.velocity = velocity;
+        grounded = rigidbody.Raycast(Vector2.down);
 
-        // Flip sprite direction depending on input
-        if (moveInput > 0f)
-            transform.localScale = new Vector3(1f, 1f, 1f);
-        else if (moveInput < 0f)
-            transform.localScale = new Vector3(-1f, 1f, 1f);
-    }
-
-    private void Jump()
-    {
-        if (Input.GetButtonDown("Jump") && !isJumping)
+        if (grounded)
         {
-            rigidbody2D.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
-            isJumping = true;
+            GroundedMovement();
+        }
+
+        ApplyGravity();
+    }
+
+    private void FixedUpdate()
+    {
+        // move mario based on his velocity
+        Vector2 position = rigidbody.position;
+        position += velocity * Time.fixedDeltaTime;
+
+        // clamp within the screen bounds
+        Vector2 leftEdge = camera.ScreenToWorldPoint(Vector2.zero);
+        Vector2 rightEdge = camera.ScreenToWorldPoint(new Vector2(Screen.width, Screen.height));
+        position.x = Mathf.Clamp(position.x, leftEdge.x + 0.5f, rightEdge.x - 0.5f);
+
+        rigidbody.MovePosition(position);
+    }
+
+    private void HorizontalMovement()
+    {
+        // accelerate / decelerate
+        inputAxis = Input.GetAxis("Horizontal");
+        velocity.x = Mathf.MoveTowards(velocity.x, inputAxis * moveSpeed, moveSpeed * Time.deltaTime);
+
+        // check if running into a wall
+        if (rigidbody.Raycast(Vector2.right * velocity.x))
+        {
+            velocity.x = 0f;
+        }
+
+        // flip sprite to face direction
+        if (velocity.x > 0f)
+        {
+            transform.eulerAngles = Vector3.zero;
+        }
+        else if (velocity.x < 0f)
+        {
+            transform.eulerAngles = new Vector3(0f, 180f, 0f);
         }
     }
 
-    // Detect collision with the ground
+    private void GroundedMovement()
+    {
+        // prevent gravity from infinitly building up
+        velocity.y = Mathf.Max(velocity.y, 0f);
+        jumping = velocity.y > 0f;
+
+        // perform jump
+        if (Input.GetButtonDown("Jump"))
+        {
+            velocity.y = jumpForce;
+            jumping = true;
+        }
+    }
+
+    private void ApplyGravity()
+    {
+        // check if falling
+        bool falling = velocity.y < 0f || !Input.GetButton("Jump");
+        float multiplier = falling ? 2f : 1f;
+
+        // apply gravity and terminal velocity
+        velocity.y += gravity * multiplier * Time.deltaTime;
+        velocity.y = Mathf.Max(velocity.y, gravity / 2f);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.contacts[0].normal.y > 0.5f)  // Check if the collision is below Mario
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Enemy"))
         {
-            isJumping = false; // Mario is on the ground
+            // bounce off enemy head
+            if (transform.DotTest(collision.transform, Vector2.down))
+            {
+                velocity.y = jumpForce / 2f;
+                jumping = true;
+            }
+        }
+        else if (collision.gameObject.layer != LayerMask.NameToLayer("PowerUp"))
+        {
+            // stop vertical movement if mario bonks his head
+            if (transform.DotTest(collision.transform, Vector2.up))
+            {
+                velocity.y = 0f;
+            }
         }
     }
+
 }
