da19334e270d55dec923ca3deacbb662ad507372
XxShuoxX <Shuo.Hu-1@student.uts.edu.au>
2024-03-02 20:54:26 +1100

    .
    Test

--- lab2/Assets/Script/CameraFollow.cs
+++ lab2/Assets/Script/CameraFollow.cs
@@ -2,6 +2,7 @@
 
 public class CameraFollow : MonoBehaviour
 {
+<<<<<<< Updated upstream
     public Transform target; // 目标物体，相机将跟随的对象
     public float smoothSpeed = 0.125f; // 相机跟随的平滑速度
     public Vector3 offset; // 相机与目标物体之间的偏移量
@@ -14,5 +15,19 @@ public class CameraFollow : MonoBehaviour
 
         // 可选：如果你想让相机始终指向目标，取消下面一行的注释
         // transform.LookAt(target);
+=======
+    public Transform target; // 玩家角色的Transform组件
+    public float smoothSpeed = 5f; // 调整此值来控制摄像机跟随速度
+    public Vector3 offset;
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            Vector3 desiredPosition = target.position + offset;
+            Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed * Time.deltaTime);
+            transform.position = smoothedPosition;
+        }
+>>>>>>> Stashed changes
     }
 }
